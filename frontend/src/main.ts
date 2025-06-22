2cc2ff0680777b6949b3992741cd5e601e6c37c2
Liu <7runv6se@duck.com>
2025-06-22 14:09:56 +0800

    fix svelte 5 api

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,6 @@
 import './app.css';
 import App from './App.svelte'
+import { mount } from 'svelte';
 
-const app = new App({
-  target: document.getElementById('app')!,
-})
+mount(App, { target: document.body });
 
-export default app
